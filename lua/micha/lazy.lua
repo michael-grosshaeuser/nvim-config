8f262c7bad658e791dd05959b3fe3de31a8c8a22
Michael Großhäuser <micgro2@gmail.com>
2025-09-23 22:24:05 +0200

    load lsp config

--- lua/micha/lazy.lua
+++ lua/micha/lazy.lua
@@ -11,4 +11,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("micha.plugins")
+require("lazy").setup({ 
+  {import = "micha.plugins"},
+  { import = "micha.plugins.lsp" } 
+}, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})

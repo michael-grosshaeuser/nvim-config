a76538e20c275cea74f956c9a33722cab7823e48
Michael Großhäuser <micgro2@gmail.com>
2025-09-27 21:53:55 +0200

    fix linting error in lualine

--- lua/micha/plugins/lualine.lua
+++ lua/micha/plugins/lualine.lua
@@ -1,22 +1,24 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local lualine = require("lualine")
-    ---local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
+		---local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-    local colors = {
-      blue = "#65D1FF",
-      green = "#3EFFDC",
-      violet = "#FF61EF",
-      yellow = "#FFDA7B",
-      red = "#FF4A4A",
-      fg = "#c3ccdc",
-      bg = "#112638",
-      inactive_bg = "#2c3043",
-    }
+		local colors = {
+			blue = "#65D1FF",
+			green = "#3EFFDC",
+			violet = "#FF61EF",
+			yellow = "#FFDA7B",
+			red = "#FF4A4A",
+			fg = "#c3ccdc",
+			bg = "#112638",
+			inactive_bg = "#2c3043",
+		}
 
-    -- configure lualine with modified theme
-    lualine.setup()
-  end
+		local user_config = {} -- Stores currently applied config
+
+		-- configure lualine with modified theme
+		lualine.setup(user_config)
+	end,
 }

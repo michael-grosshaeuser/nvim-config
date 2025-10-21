a78d22889d5f25bd8db29434f88befe7a29e3877
Michael Großhäuser <160682946+michael-grosshaeuser@users.noreply.github.com>
2025-10-21 20:09:09 +0200

    docs: change comment

--- lua/micha/lsp.lua
+++ lua/micha/lsp.lua
@@ -23,7 +23,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
 		opts.desc = "See available code actions"
-		keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+		keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- in visual mode will apply to selection
 
 		opts.desc = "Smart rename"
 		keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
